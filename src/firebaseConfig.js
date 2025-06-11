6fba3c9ddc32e00abdd8dcdb2356f8040e55f33f
Nachiket Amlekar <nachiketamlekar@Nachikets-MacBook-Pro.local>
2025-06-11 16:35:45 +0530

    final

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBUxl-PffrI9ZmkahX3ey8V_gqjCgJf7Mk",
   authDomain: "chefgpt-4705b.firebaseapp.com",
   projectId: "chefgpt-4705b",
-  storageBucket: "chefgpt-4705b.firebasestorage.app",
+  storageBucket: "chefgpt-4705b.appspot.com",
   messagingSenderId: "792044693275",
   appId: "1:792044693275:web:02112a733f25d6594055be",
   measurementId: "G-NFWFGPPHB3"
